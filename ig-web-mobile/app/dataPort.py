25591aafbc0754a86818eee8320041c950df5fd7
gaoqi <1819561335@qq.com>
2019-11-12 09:31:13 +0000

    Update dataPort.py

--- ig-web-mobile/app/dataPort.py
+++ ig-web-mobile/app/dataPort.py
@@ -1,20 +1,11 @@
 class dataPort():
     page_dataport="http://10.132.166.121:5813";
     part_login=page_dataport+"/web/sys/login";
-    # part_login=page_dataport+"/safety/user/login";
-    # part_login=page_dataport+"/track/account/checkLoginAccount";
     #安防主题  
     part_doorlog = page_dataport + "/safety/doorLog/userList";#一线门岗人流
     part_doorlogT = page_dataport + "/safety/statistics/userListHour";#一线门岗人流-时间
-    # part_cardoorlog = page_dataport + "/safety/doorLog/carList";#一线门岗车流
-    # part_cardoorlogT = page_dataport + "/safety/statistics/carListHour";#一线门岗车流-时间
     part_facepic = page_dataport + "/safety/device/getDeviceCount";#设备抓拍
-    # part_carType = page_dataport + "/safety/statistics/carTypeList";#车辆类型数据
-    # part_security_warning = page_dataport + "/safety/alarm/list";#安防警告
-    # part_secuindex = page_dataport + "/safety/statistics/index";#安全指数
-    # part_deviceDetail = page_dataport + "/safety/device/detail";#安防设备详情
     # 人資视图
-    # part_user = page_dataport + "/safety/humancapital/userList";#最近12個月人力分布
     part_protitle = page_dataport + "/safety/humancapital/zcList";#职称分布
     part_staff = page_dataport + "/safety/humancapital/ygList";#用工类型
     # 綜合人流視圖
@@ -28,32 +19,7 @@ class dataPort():
     part_userDis = page_dataport + "/safety/boss/userDis" #人力整体分布
     part_safe = page_dataport + "/safety/boss/safe" #安全分布
     part_onduty = page_dataport + "/safety/humancapital/index" #考勤
-    # part_iBrain = page_dataport + "/safety/boss/head" #园区智脑
-    # part_personTrack = "http://10.132.166.121:8022/track/appToAbility/getEmployTarckList/1/40"
-    part_personTrack = "http://10.132.166.121:5813/others/track/appToAbility/getEmployTrackList/1/40"
-    # part_personTrack = page_dataport +"/track/appToAblity/getEmployTarckList/1/15"
-    #运维主题
-    # part_sysStatus = page_dataport + "/safety/oper/status" #平台状态
-    # part_sysAccess = page_dataport + "/safety/oper/accessStatus" #接入层状态
-    # part_sysLog = page_dataport + "/safety/oper/log" #运维日志
-    # part_sysdataStatus = page_dataport + "/safety/oper/dataStatus" #数据状态
-    # part_sysdataCount = page_dataport + "/safety/oper/dataCount" #数据统计
-    # # part_sysdeviceStatus = page_dataport + "/safety/oper/deviceStatus" #设备状态
-    # part_sysmicroStatus = page_dataport + "/safety/oper/wStatus" #微服务调用情况
-    # part_sysOther = page_dataport + "/safety/oper/otherData" #其他服务
+    part_personTrack =  page_dataport + "/others/track/appToAbility/getEmployTrackList/1/40"
     # 地图接口
-    part_persontrail = "http://10.132.166.121:5813/others/employs/getEmploysInfos" #人员详情
-    part_doorDetail = "http://10.132.166.121:5813/others/employs/getGatesInOut" #人员详情
-    # part_persontrail = page_dataport + "/safety/person/getEmployDetail"#人员详情
-    # part_persontimetrail = page_dataport + "/safety/person/getTrail"#人员轨迹
-    # part_deviceList = page_dataport + "/safety/device/list" #设备信息接口
-
-    # part_attentionTrail=page_dataport+"/web/trail/getMyFollowPerson"
-    # part_attentionTrail=page_dataport + "/safety/appUser/getMyFollow"
-
-    # http://10.132.166.121:5813/web/area/getDepartList
-    # http://10.132.166.121:5813/web/trail/getMyFollowPerson
-    # http://10.132.166.121:5813/web/trail/getFollowPerson
-    # http://10.132.166.121:5813/web/trail/delMyFollowPerson
-    # http://10.132.166.121:5813/web/trail/saveMyFollowPerson
-    # http://10.132.166.121:5813/web/trail/getPersonTrailList
+    part_persontrail =  page_dataport + "/others/employs/getEmploysInfos" #人员详情
+    part_doorDetail =  page_dataport + "/others/employs/getGatesInOut" #人员详情
